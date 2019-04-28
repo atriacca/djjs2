12bc7a635f38b31fde495497fb996eb1a91cc41e
Alan Triacca <atriacca@yahoo.com>
2019-04-28 10:04:33 -0600

    first commit

---
+++ ref.js
@@ -0,0 +1,26 @@
+// You're starting your business as a strictly online DJ (DJ JS) and you want to start by creating a website where people can change the color of a square on the page by interacting with different HTML Events.
+
+// The square's color will change as follows:
+
+// Blue when the mouse hovers over the square
+// Red when the mouse button is held down over the square
+// Yellow when the mouse button is let go over the square
+// Green when the mouse is double clicked in the square
+// Orange when the mouse scroll is used somewhere in the window (not just over the square).
+// You should also be able to press the first letter of the colors (typing "r" on the keyboard for "red", for example) and have the box change to that color. Check this site for a full list of keyboard key codes.
+
+// This time around, we won't be focusing on javascript event listeners though. Instead, every event will happen on hover, which you can just use the CSS :hover pseudo-selector for. In other words, no JavaScript file required this time.
+
+// Also, instead of one main box, you'll make 6 smaller boxes. Each one, when hovered, should change 2 or 3 different properties of the box that was hovered.
+
+// You get to choose which properties change, but try to vary it between each box. Here's a non-exhaustive list of something you could change about each box:
+
+// Background color
+// Border (width, color, style)
+// Border radius (can target each corner individually if you want)
+// Box shadow
+// Height/width
+// Margin/padding (put some text inside to better see what's happening)
+// Position
+// Opacity
+// ALSO, you must make each box smoothly transition from one style to another. (This is a CSS transitions practice, after all).
